eb860379214d04b5940e9667e8571f002d679d06
Bruno Luiz <50926585+BRL4528@users.noreply.github.com>
2020-04-07 07:58:47 -0400

    update URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.github.com',
+  baseURL: 'https://nfe.api.nfe.io/v2/productinvoices'
+//  baseURL: 'https://api.github.com',
 });
 
 export default api;
